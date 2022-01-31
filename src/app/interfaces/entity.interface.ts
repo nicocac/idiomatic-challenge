8ee4411fb35daada303653371aa8183706592049
Nicolas.estevez <nicolas.estevez@corp.doctor.com>
2022-01-31 16:11:16 -0300

    first commit

---
+++ src/app/interfaces/entity.interface.ts
@@ -0,0 +1,6 @@
+export interface entity {
+  id: number;
+  name: string;
+  surname: string;
+  selected?: boolean;
+}
---
+++ src/app/interfaces/options.interface.ts
@@ -0,0 +1,5 @@
+export interface options {
+  create: boolean;
+  update: boolean;
+  delete: boolean;
+}
